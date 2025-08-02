7c06d0223deae2a97158344537584510fb53f166
Spencer <92634339+Cloud-Chemist@users.noreply.github.com>
2025-08-02 19:19:23 -0400

    Add files via upload

---
+++ youtubeplayer.py
@@ -0,0 +1,17 @@
+# youtube_player.py
+from youtubesearchpython import VideosSearch
+import pafy
+import vlc
+
+def play_youtube(query):
+    search = VideosSearch(query, limit=1).result()
+    if not search['result']:
+        print("No results.")
+        return
+    url = search['result'][0]['link']
+    video = pafy.new(url)
+    best = video.getbest()
+    playurl = best.url
+    player = vlc.MediaPlayer(playurl)
+    player.play()
+    print(f"Playing: {search['result'][0]['title']}")
